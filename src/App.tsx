9a54bfbc571faa197f49108177db26d1ca0b0ca6
Monu_2114 <monu.nanabala@gmail.com>
2024-12-12 22:46:39 +0530

    bug 5

--- src/App.tsx
+++ src/App.tsx
@@ -29,9 +29,9 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
-    await paginatedTransactionsUtils.fetchAll()
-
     setIsLoading(false)
+
+    await paginatedTransactionsUtils.fetchAll()
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
